da7505095650e8ed7ebf0445cc808dce9e39c3c8
tamilarasimailsamy <tamilarasi2002k@gmail.com>
2025-04-07 13:21:33 +0530

    create_string

---
+++ String_collcetion/create_string/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let s = String::new();
+   // let name= "Vetri";
+   // let s =name.to_string();
+
+
+    // the method also works on a literal directly:
+   // let s = "initial contents".to_string();
+
+
+   //we can use String::from function to create string
+     let s=String::from("Vetrivel");
+     
+
+    println!("The name is{:?}",s);
+}
