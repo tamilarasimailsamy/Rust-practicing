e18f88756b981555d12d658a79a71e2bc45ed15a
tamilarasimailsamy <tamilarasi2002k@gmail.com>
2025-04-07 13:22:45 +0530

    UTF-8string

---
+++ String_collcetion/UTF-8string/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let hello1 = String::from("السلام عليكم");
+    let hello2 = String::from("Dobrý den");
+    let hello3= String::from("Hello");
+    let hello4 = String::from("שלום");
+    let hello5 = String::from("नमस्ते");
+    let hello6 = String::from("こんにちは");
+    let hello7 = String::from("안녕하세요");
+    let hello8 = String::from("你好");
+    let hello9 = String::from("Olá");
+    let hello10 = String::from("Здравствуйте");
+    let hello11 = String::from("Hola");
+    println!("The main function has string{:?}",hello1);
+    println!("The main function has string{:?}",hello2);
+    println!("The main function has string{:?}",hello3);
+    println!("The main function has string{:?}",hello4);
+    println!("The main function has string{:?}",hello5);
+    println!("The main function has string{:?}",hello6);
+    println!("The main function has string{:?}",hello7);
+    println!("The main function has string{:?}",hello8);
+}
