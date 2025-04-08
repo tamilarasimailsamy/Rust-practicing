6ac1b5c2d38c10d6ab98ead08492464a16410858
tamilarasimailsamy <tamilarasi2002k@gmail.com>
2025-04-08 10:26:42 +0530

    accessing_values

--- hashMap_collection/accessing_values/src/main.rs
+++ hashMap_collection/accessing_values/src/main.rs
@@ -1,16 +1,10 @@
-
 fn main(){
+    use std::collections::HashMap;
 
-
-use std::collections::HashMap;
-
-let mut scores = HashMap::new();
-
-scores.insert(String::from("Blue"), 10);
-scores.insert(String::from("Yellow"), 50);
-
-let team_name = String::from("Yellow");
-let score = scores.get(&team_name).copied().unwrap_or(0);
-
-println!("{:?}",score);
+    let mut scores=HashMap::new();
+    scores.insert(String::from("blue"),10);
+    scores.insert(String::from("green"),30);
+    for(key,values)in &scores{
+        println!("{key}:{values}");
+    }
 }
